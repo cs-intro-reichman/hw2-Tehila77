79ebaf7a39dc0a65d7ee06b7acd600a20da6b41e
Tehila77 <t.alaloof77@gmail.com>
2024-11-17 13:45:09 +0200

    try run

--- Cheers.java
+++ Cheers.java
@@ -3,27 +3,21 @@ public class Cheers {
         public static void main(String[] args) {
 	    String theWord=args[0];
             int countCherring=Integer.parseInt(args[1]);
-            System.out.println(theWord +countCherring);
+            String checkWord="AEFHILMNORSX";
             for(int i=0;i<theWord.length();i++)
             {
-                for(int j=0;j<12;j++)
-                {
-                String checkWord="AEFHILMNORSX";
-                if(theWord.charAt(i)==checkWord.charAt(i))
+                int theChar=theWord.charAt(i);
+                if(checkWord.indexOf(theChar)>=0)
                 {
                  System.out.println("Give me an "+ theWord.charAt(i)+": "+ theWord.charAt(i)+"! ");     
-                }
-                else System.out.println("Give me a "+ theWord.charAt(i)+": "+ theWord.charAt(i)+"! ");
+                } else { System.out.println("Give me a "+ theWord.charAt(i)+": "+ theWord.charAt(i)+"! ");
                 }
             }
-            /* 
+        //prith the part 2
             System.out.println("What does that spell?");
             for(int i=0;i<countCherring;i++)
-            {
-             System.out.println(theWord+" ! ! ! ");   
-           }
-            */
-            
+             System.out.println(theWord+" ! ! !");   
+          
         }
 
 

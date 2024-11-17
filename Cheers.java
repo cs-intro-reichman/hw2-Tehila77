1f59c39c4892a1e729765ace7bab252e2eb19307
Tehila77 <t.alaloof77@gmail.com>
2024-11-17 17:26:54 +0200

    try run

--- Cheers.java
+++ Cheers.java
@@ -18,7 +18,6 @@ public class Cheers {
         System.out.println("What does that spell?");
         for (int i = 0; i < countCherring; i++)
             System.out.println(theWord + " ! ! !");
-
     }
 
 }

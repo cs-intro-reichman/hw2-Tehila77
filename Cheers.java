db8b4a41cee04f4f51f5d3857147e6d2a949d5a8
Tehila77 <t.alaloof77@gmail.com>
2024-11-17 13:21:24 +0200

    try the first step

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,30 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String theWord=args[0];
+            int countCherring=Integer.parseInt(args[1]);
+            System.out.println(theWord +countCherring);
+            for(int i=0;i<theWord.length();i++);
+            {
+                for(int j=0;j<12;j++)
+                {
+                String checkWord="AEFHILMNORSX";
+                if(theWord.charAt('i')==checkWord.charAt('i'))
+                {
+                 System.out.println("Give me an "+ theWord.charAt('i'));     
+                }
+                else System.out.println("Give me a "+ theWord.charAt('i'));
+                }
+            }
+            /* 
+            System.out.println("What does that spell?");
+            for(int i=0;i<countCherring;i++)
+            {
+             System.out.println(theWord+" ! ! ! ");   
+           }
+            */
+            
         }
+
+
 }
